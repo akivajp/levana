dddf3326293cd570e1c22042f1dd98c9c3742f04
Akiva Miura <akiva.miura@gmail.com>
2011-01-04 17:56:20 +0200

    Makefile fixes

---
+++ trunk/make/levana.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+PWD=`pwd`
+DIR=`dirname $0`
+
+if [ $PWD = "/" ]
+then
+  PWD=""
+fi
+if [ $DIR = "." ]
+then
+  DIR=""
+fi
+
+BIN_DIR=${PWD}${DIR}
+CON_DIR=`dirname $BIN_DIR`
+APP_DIR=`dirname $CON_DIR`
+WORK_DIR=`dirname $APP_DIR`
+cd $BIN_DIR
+./levana
+#./levana $BIN_DIR/entry.lua
+exit 0
+
